aeffe3d20e893018e537d68e492dbcdbbbcfe640
MatheusVMariussi <matheus.mariussi@pucpr.edu.br>
2025-12-04 13:26:21 -0300

    agora vai

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import DetailsPage from './pages/DetailsPage';
 import FormPage from './pages/FormPage';
-import './styles.css';
-
-import { AuthProvider } from './contexts/AuthContext';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { AuthProvider } from './contexts/AuthContext';
+import './styles.css';
 
 function App() {
   return (
@@ -20,8 +20,9 @@ function App() {
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/skins/:id" element={<DetailsPage />} />
-              <Route path="/anunciar" element={<FormPage />} /> 
+              <Route path="/anuncio/:id" element={<DetailsPage />} />
+              <Route path="/novo-anuncio" element={<FormPage />} />
+              <Route path="/editar-anuncio/:id" element={<FormPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
             </Routes>
